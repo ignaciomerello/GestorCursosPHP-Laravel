2624d6cfc4f71433d34dd6b899a4db39c2c3ba89
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-11-26 12:50:37 +0100

    Add Model Curso

---
+++ gestorCusrosAlumnos/database/seeders/CursoSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Curso;
+use Illuminate\Database\Seeder;
+
+
+class CursoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Curso::factory(15)->create();
+    }
+}
